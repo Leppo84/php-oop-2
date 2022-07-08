d9271f4ce59c22687f5df81c8a4875c17a7b0e54
LeppoXena <francesco.lepore@xenagames.com>
2022-07-08 15:47:32 +0200

    Class article added

---
+++ Product.php
@@ -0,0 +1,79 @@
+<?php
+
+class Product {
+    protected string $prodName;
+    protected string $sellerBrand;
+    protected int $artId;
+    protected int $stockQuantity;
+    protected int $requestedQuantity;
+    protected string $priceUnit;
+    protected float $sellingPrice;
+    protected float $volume;
+    protected float $weight;
+
+    public function __construct($prodName, $sellerBrand, $artId,$stockQuantity, $requestedQuantity, $priceUnit, $sellingPrice, $volume, $weight) {
+        $this->prodName = $prodName;
+        $this->sellerBrand = $sellerBrand;
+        $this->artId = $artId;
+        $this->stockQuantity = $stockQuantity;
+        $this->requestedQuantity = $requestedQuantity;
+        $this->priceUnit = $priceUnit;
+        $this->sellingPrice = $sellingPrice;
+        $this->volume = $volume;
+        $this->weight = $weight;
+    }
+
+    public function setProdName($prodName)
+    {
+        $this->prodName = $prodName;
+        return $this;
+    }
+
+    public function setSellerBrand($sellerBrand)
+    {
+        $this->sellerBrand = $sellerBrand;
+        return $this;
+    }
+    
+    public function setArtId($artId)
+    {
+        $this->artId = $artId;
+        return $this;
+    }
+
+    public function setstockQuantity($stockQuantity)
+    {
+        $this->stockQuantity = $stockQuantity;
+        return $this;
+    }
+
+    public function setrequestedQuantity($requestedQuantity)
+    {
+        $this->requestedQuantity = $requestedQuantity;
+        return $this;
+    }
+
+    public function setpriceUnit($priceUnit)
+    {
+        $this->priceUnit = $priceUnit;
+        return $this;
+    }
+
+    public function setsellingPrice($sellingPrice)
+    {
+        $this->sellingPrice = $sellingPrice;
+        return $this;
+    }
+
+    public function setVolume($volume)
+    {
+        $this->volume = $volume;
+        return $this;
+    }
+
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+        return $this;
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include __DIR__ . '/Product.php';
+include __DIR__ . '/Food.php';
+include __DIR__ . '/Toy.php';
+include __DIR__ . '/Accessory.php';
+
+//     public function __construct($prodName, $artId,$stockQuantity, $priceUnit, $sellingPrice, $volume, $weight)
+
